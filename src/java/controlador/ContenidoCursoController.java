fae4531bf183ec84fe198a524496d5afeaa01b20
JosueEMG <josueomega355@gmail.com>
2020-10-19 01:05:49 -0500

    Cambios en paquete modelo, vista a contenido_curso, cambios en factura e inscripcion. Controladores Contenidocurso inscricpion  y factura

--- src/java/controlador/ContenidoCursoController.java
+++ src/java/controlador/ContenidoCursoController.java
@@ -6,25 +6,28 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
-import modelo.video;
+import modelo.contenido_curso;
 import util.MySQLConexion;
 
-public class VideoController {
+public class ContenidoCursoController {
     
-    public List<video> lisVideo(int id) {
-        List<video> lis = new ArrayList<>();
+    public List<contenido_curso> listarContenidoCurso(int idCurso) {
+        List<contenido_curso> lis = new ArrayList<>();
         Connection conn = null;
 
         try {
             conn = MySQLConexion.getConexion();
-            String sql = "select link from video where id_curso=?";
+            String sql = "select id_contenido, id_curso, archivo, enlace from contenido_curso where id_curso=?";
             PreparedStatement st = conn.prepareStatement(sql);
-            st.setInt(1, id);
+            st.setInt(1, idCurso);
             ResultSet rs = st.executeQuery();
             //llenar el arraylist con la clase entidad
             while (rs.next()) {
-                video v = new video();
-                v.setLink(rs.getString(1));
+                contenido_curso v = new contenido_curso();
+                v.setId_contenido(rs.getInt(1));
+                v.setId_curso(rs.getInt(2));
+                v.setArchivo(rs.getString(3));
+                v.setLink(rs.getString(4));
                 lis.add(v);
             }
         } catch (Exception ex) {
@@ -40,7 +43,7 @@ public class VideoController {
         }
         return lis;
     }
-    
+    /*
     public void AdicionVideo(int id, String url) {
         Connection conn = null;
         
@@ -65,17 +68,17 @@ public class VideoController {
             }
         }
     }
-    
-    public void BorrarVideo(int id) {
+    */
+    public void BorrarEnlaceContenido(int idContenido) {
         Connection conn = null;
         
         try {
             conn = MySQLConexion.getConexion();
-            String sql = "delete from video where id_video=?";
+            String sql = "update contenido_curso set enlace='' where id_contenido=?";
             //? =equivale a un parametro 
             PreparedStatement st = conn.prepareStatement(sql);
             //relacionar el ? con su variable 
-            st.setInt(1, id);
+            st.setInt(1, idContenido);
             st.executeUpdate();
         } catch (Exception ex) {
             ex.printStackTrace();
@@ -89,7 +92,7 @@ public class VideoController {
             }
         }
     }
-    
+    /*
     public void BorrarVideoxCurso(int id) {
         Connection conn = null;
         
@@ -112,5 +115,5 @@ public class VideoController {
             } catch (Exception e2) {
             }
         }
-    }
+    }*/
 }
