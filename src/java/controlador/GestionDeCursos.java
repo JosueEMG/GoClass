ef48a9b17a45f9d82e61de818425056ea2a99168
juanguz1999 <55995911+juanguz1999@users.noreply.github.com>
2020-10-24 16:25:50 -0500

    Agregar contenido

--- src/java/controlador/GestionDeCursos.java
+++ src/java/controlador/GestionDeCursos.java
@@ -1,14 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controlador;
 
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import util.MySQLConexion;
+import modelo.*;
 /**
  *
  * @author JuanG
  */
 public class GestionDeCursos {
+        public void AgregarContenido(contenido_curso a) {
+          Connection conn = null;
+        try {
+            conn = MySQLConexion.getConexion();
+            String sql = "{call sp_adicion(?,?,?,?)}";
+            //? =equivale a un parametro 
+            CallableStatement st = conn.prepareCall(sql);
+            //relacionar el ? con su variable 
+            st.setInt(1, a.getId_contenido());
+            st.setInt(2, a.getId_curso());
+            st.setString(3, a.getArchivo());
+            st.setString(4, a.getLink());
+            st.executeUpdate();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (Exception e2) {
+            }
+        }
+
+    }
     
 }
