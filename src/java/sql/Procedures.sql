3b7060ce772592bde90c1fea91b95f77587c1c8c
juanguz1999 <55995911+juanguz1999@users.noreply.github.com>
2020-10-24 22:15:04 -0500

    Procedure sin error

--- src/java/sql/Procedures.sql
+++ src/java/sql/Procedures.sql
@@ -4,6 +4,7 @@ DROP PROCEDURE IF EXISTS AdicionCurso @@
 CREATE PROCEDURE AdicionCurso(nombrec varchar(45),  precio numeric(10,2), banner varchar(255),idesp int,idusuario int,detalle varchar(255), inscripciones int)
 BEGIN
 declare nro int;
+declare idcur int;
 select ifnull(max(idcurso),0) + 1  from curso into nro;
 set idcur = lpad(nro,11,'0');
 insert into facdet values (idcur,nombrec,precio,banner,idesp,idusuario,detalle,inscripciones);
@@ -13,9 +14,8 @@ END@@
 DROP PROCEDURE IF EXISTS AdicionContenido @@
 CREATE PROCEDURE AdicionContenido(archivo varchar(255), linkpag varchar(255))
 BEGIN
-declare nros int;
-select ifnull(max(id_contenido),0) + 1  from contenido_curso into nros;
-set idcon = nros;
+declare idcon int;
+set idcon = (select max(id_contenido) + 1  from contenido_curso);
 insert into contenido_curso values (idcon,idcur,archivo,linkpag);
 END@@
 DELIMITER ;
